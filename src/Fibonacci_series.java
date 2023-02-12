0a51b4ce514286bc31572a30bfee63dcaf6e1b01
Swanand-Yadav <swanandtyadav@gmail.com>
2023-02-12 18:43:32 +0530

    added

--- src/Fibonacci_series.java
+++ src/Fibonacci_series.java
@@ -3,15 +3,14 @@ public class Fibonacci_series
 {
 	public static void main(String args[])
     {
-    	int n1=0,n2=1,n3,i,count=10;    
-    	System.out.print(n1+" "+n2);//printing 0 and 1    
-    
-    	for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
-    	{    
-    		n3=n1+n2;    
-    		System.out.print(" "+n3);    
-    		n1=n2;    
-    		n2=n3;    
-    	}    
+		int a=0, b=0, c=1;
+        
+        for (int i=0;i<10;i++) 
+        {
+            a = b;
+            b = c;
+            c = a+b;
+            System.out.print(a+" ");    
+        }  
     }
 }
