d6742e86e67a8bc6b35e441dfeb1ea9e3bc51808
Swanand-Yadav <swanandtyadav@gmail.com>
2023-02-22 11:05:16 +0530

    added

--- src/Armstrong_number_or_not.java
+++ src/Armstrong_number_or_not.java
@@ -1,9 +1,25 @@
 
-public class Armstrong_number_or_not {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Armstrong_number_or_not 
+{
+	public static void main(String[] args) 
+	{
+		int n = 1634;
+        int temp = n;
+        int p = 0;
+ 
+        while (n > 0) 
+        {
+            int rem = n % 10;
+            p = p + (rem * rem * rem);
+            n = n / 10;
+        }
+ 
+        if (temp == p) {
+            System.out.println("Yes. It is Armstrong No.");
+        }
+        else {
+            System.out.println(
+                "No. It is not an Armstrong No.");
+        }
 	}
-
 }
